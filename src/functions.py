af41fd8937a317db231b500a509f0622307edb67
ThanosSkentzos <kalligerist@gmail.com>
2024-08-19 22:44:36 +0300

    add split link and image

--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,5 @@
 import re
+from unittest.runner import TextTestRunner
 
 from textnode import (
     TextNode,LeafNode,
@@ -42,15 +43,60 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             new_nodes.append(node)
         else:
             #find new nodes from textnode
-            text=node.text
+            text = node.text
             num_delimiter_found = text.count(delimiter)
             if num_delimiter_found%2!=0:
-                print(text.count(delimiter))
                 raise Exception("Invalid Markdown Syntax")
             else:
                 return split_delimiters(text,delimiter)
+def split_nodes_image(old_nodes):
+    nodes = []
+    for node in old_nodes:
+        text = node.text
+        img_tuples = extract_markdown_images(text)
+        if len(img_tuples)==0:
+            nodes += [TextNode(text,text_type_text)]
+        remaining_text = text
+        for t in img_tuples:
+            alt = t[0]
+            url = t[1]
+            img_text = f"![{alt}]({url})"
+            before,after = remaining_text.split(img_text,1)
+            if len(before)!=0:
+                nodes += [TextNode(before,text_type_text)]
+            nodes += [TextNode(alt,text_type_image,url)]
+            remaining_text = after
+        if len(remaining_text)>0:
+            nodes += [TextNode(remaining_text,text_type_text)]
+    return nodes
+
+def split_nodes_link(old_nodes):
+    nodes = []
+    for node in old_nodes:
+        text = node.text
+        link_tuples = extract_markdown_links(text)
+        if len(link_tuples)==0:
+            nodes += [TextNode(text,text_type_text)]
+        remaining_text = text
+        c = 0
+        for t in link_tuples:
+            c+=1
+            alt = t[0]
+            url = t[1]
+            link_text = f"[{alt}]({url})"
+            before,after = remaining_text.split(link_text,1)
+            if len(before)!=0:
+                nodes += [TextNode(before,text_type_text)]
+            nodes += [TextNode(alt,text_type_link,url)]
+            remaining_text = after
+        if len(remaining_text)>0:
+            nodes += [TextNode(remaining_text,text_type_text)]
+    return nodes
+
 
 def split_delimiters(text,delim) -> list[TextNode]:
+    if len(text)==0:
+        return []
     if delim not in text:
         return [TextNode(text,text_type_text)]
     type = delim_to_text_type[delim]
@@ -58,7 +104,7 @@ def split_delimiters(text,delim) -> list[TextNode]:
     lst = [TextNode(before,text_type_text),TextNode(inside,type)]
     if len(after)>3:
         lst += split_delimiters(after,delim)
-    else:
+    elif len(after)>0:
         lst += [TextNode(after,text_type_text)]
     return lst
 
@@ -67,13 +113,14 @@ def extract_markdown_images(text):
 
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)",text)
-def main():
-    text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
-    print(extract_markdown_images(text))
-    # [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
 
-    text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev) but not ![rick roll](https://i.imgur.com/aKaOqIh.gif)"
-    print(extract_markdown_links(text))
-    # [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+
+def main():
+    node = TextNode(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+        text_type_text,
+    )
+    new_nodes = split_nodes_image([node])
+    print(new_nodes)
 if __name__=="__main__":
     main()
